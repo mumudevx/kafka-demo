1b638b6f5c2c2316cf592a1660eda95b9483fa23
mumudevx <mumudevx@gmail.com>
2024-12-01 17:42:40 +0300

    Update
    - Producer and Consumer apps were created.

---
+++ ApacheKafkaDemo/KafkaConsumerApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appSettings.json")
+    .Build();
+
+var bootstrapServers = config["Kafka:BootstrapServers"];
+var topic = config["Kafka:TopicName"];
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = bootstrapServers,
+    GroupId = "kafka-demo-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+
+consumer.Subscribe(topic);
+Console.WriteLine("Consuming messages... (press Ctrl+C to quit)");
+
+try
+{
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Closing consumer.");
+    consumer.Close();
+}
---
+++ ApacheKafkaDemo/KafkaProducerApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appSettings.json")
+    .Build();
+
+var bootstrapServers = config["Kafka:BootstrapServers"] ??
+                       throw new Exception("BootstrapServers cannot read from appSettings.json");
+
+var topic = config["Kafka:TopicName"] ??
+            throw new Exception("TopicName cannot read from appSettings.json");
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = bootstrapServers
+};
+
+Console.WriteLine("Enter messages to send Kafka (type 'exit' to quit):");
+
+using var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+
+string? message;
+
+while ((message = Console.ReadLine()) != "exit")
+{
+    try
+    {
+        var deliveryResult = await producer
+            .ProduceAsync(topic, new Message<Null, string> { Value = message });
+
+        Console.WriteLine($"Delivered '{deliveryResult.Value}' to '{deliveryResult.TopicPartitionOffset}'");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"Error producing message: {exception.Message}");
+        throw;
+    }
+}
